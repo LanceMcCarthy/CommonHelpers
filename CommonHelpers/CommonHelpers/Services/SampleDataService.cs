237de7dd8906479a060c1dfd48f0b51ffb1d1ed5
Lance McCarthy <lance.mccarthy@telerik.com>
2018-06-20 20:05:16 -0400

    Updated SampleDataService

--- CommonHelpers/CommonHelpers/Services/SampleDataService.cs
+++ CommonHelpers/CommonHelpers/Services/SampleDataService.cs
@@ -7,15 +7,8 @@ namespace CommonHelpers.Services
 {
     public class SampleDataService
     {
-        private readonly Random rand;
-        private List<string> peopleNames;
-
-        public SampleDataService()
-        {
-            rand = new Random();
-            LoadSharedData();
-        }
-
+        private readonly Random rand = new Random();
+        
         public IEnumerable<ChartDataPoint> GenerateCategoricalData(int count = 5)
         {
             return Enumerable.Range(1, count).Select(i => new ChartDataPoint
@@ -58,10 +51,10 @@ namespace CommonHelpers.Services
         {
             return Enumerable.Range(1, 43).Select(i => new Person
             {
-                Name = peopleNames[i],
-                Age = i + 10,
-                Gender = (GenderType)(int)rand.Next(0,3),
-                DateOfBirth = DateTime.Now.AddYears(-i)
+                Name = $"Person {i}",
+                Age = rand.Next(0,100),
+                Gender = i % 2 == 0 ? GenderType.Male : GenderType.Female,
+                DateOfBirth = DateTime.Today.AddYears(-i)
             });
         }
 
@@ -70,55 +63,52 @@ namespace CommonHelpers.Services
             return new List<string>(peopleNames);
         }
 
-        private void LoadSharedData()
+        private readonly string[] peopleNames = new string[]
         {
-            peopleNames = new List<string>()
-            {
-                "Freda Curtis",
-                "Jeffery Francis",
-                "Eva Lawson",
-                "Emmett Santos",
-                "Theresa Bryan",
-                "Jenny Fuller",
-                "Terrell Norris",
-                "Eric Wheeler",
-                "Julius Clayton",
-                "Alfredo Thornton",
-                "Roberto Romero",
-                "Orlando Mathis",
-                "Eduardo Thomas",
-                "Harry Douglas",
-                "Parker Blanton",
-                "Leanne Motton",
-                "Shanti Osborn",
-                "Merry Lasker",
-                "Jess Doyon",
-                "Kizzie Arjona",
-                "Augusta Hentz",
-                "Tasha Trial",
-                "Fredda Boger",
-                "Megan Mowery",
-                "Hong Telesco",
-                "Inez Landi",
-                "Taina Cordray",
-                "Shantel Jarrell",
-                "Soo Heidt",
-                "Rayford Mahon",
-                "Jenny Omarah",
-                "Denita Dalke",
-                "Nida Carty",
-                "Sharolyn Lambson",
-                "Niki Samaniego",
-                "Rudy Jankowski",
-                "Matha Whobrey",
-                "Jessi Knouse",
-                "Vena Rieser",
-                "Roosevelt Boyce",
-                "Kristan Swiney",
-                "Lauretta Pozo",
-                "Jarvis Victorine",
-                "Dane Gabor"
-            };
-        }
+            "Freda Curtis",
+            "Jeffery Francis",
+            "Eva Lawson",
+            "Emmett Santos",
+            "Theresa Bryan",
+            "Jenny Fuller",
+            "Terrell Norris",
+            "Eric Wheeler",
+            "Julius Clayton",
+            "Alfredo Thornton",
+            "Roberto Romero",
+            "Orlando Mathis",
+            "Eduardo Thomas",
+            "Harry Douglas",
+            "Parker Blanton",
+            "Leanne Motton",
+            "Shanti Osborn",
+            "Merry Lasker",
+            "Jess Doyon",
+            "Kizzie Arjona",
+            "Augusta Hentz",
+            "Tasha Trial",
+            "Fredda Boger",
+            "Megan Mowery",
+            "Hong Telesco",
+            "Inez Landi",
+            "Taina Cordray",
+            "Shantel Jarrell",
+            "Soo Heidt",
+            "Rayford Mahon",
+            "Jenny Omarah",
+            "Denita Dalke",
+            "Nida Carty",
+            "Sharolyn Lambson",
+            "Niki Samaniego",
+            "Rudy Jankowski",
+            "Matha Whobrey",
+            "Jessi Knouse",
+            "Vena Rieser",
+            "Roosevelt Boyce",
+            "Kristan Swiney",
+            "Lauretta Pozo",
+            "Jarvis Victorine",
+            "Dane Gabor"
+        };
     }
 }
