cfde247004473fb2eff0069d1ffc1c0987798ac3
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-01-20 14:44:01 -0500

    Update HttpClientExtensionsTests.cs

--- src/CommonHelpers.Tests/ExtensionsTests/HttpClientExtensionsTests.cs
+++ src/CommonHelpers.Tests/ExtensionsTests/HttpClientExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Threading;
@@ -12,27 +13,30 @@ namespace CommonHelpers.Tests.ExtensionsTests
     [TestClass]
     public class HttpClientExtensionsTests
     {
-        private readonly HttpClient client = new();
-
         [TestMethod]
         public async Task DownloadStringWithProgress()
         {
             // Arrange
             var reporter = new Progress<DownloadProgressArgs>();
-            float progress = 0;
-            const string url = "https://httpbin.org/encoding/utf8";
+            int progress = 0;
+            const string url = "https://raw.githubusercontent.com/LanceMcCarthy/CommonHelpers/main/src/CommonHelpers.Tests/ExtensionsTests/HttpClientExtensionsTests.cs";
 
             // Act
             reporter.ProgressChanged += (s, e) =>
             {
-                progress = e.PercentComplete;
+                progress = (int)e.PercentComplete;
             };
 
+            var client = new HttpClient();
             var result = await client.DownloadStringWithProgressAsync(url, reporter);
 
+            Trace.WriteLine($"Progress After Completion: {progress}");
+
             // Assert
-            Assert.IsTrue(progress > 0, "DownloadStringWithProgress - progress was not incremented");
+            Assert.IsTrue(progress == 100, "DownloadStringWithProgress - progress did not reach 100% completion");
             Assert.IsFalse(string.IsNullOrEmpty(result), "String result was null");
+
+            client.Dispose();
         }
 
         [TestMethod]
@@ -40,22 +44,27 @@ namespace CommonHelpers.Tests.ExtensionsTests
         {
             // Arrange
             var cts = new CancellationTokenSource();
-            float progress = 0;
-            const string url = "https://httpbin.org/encoding/utf8";
+            int progress = 0;
+            const string url = "https://raw.githubusercontent.com/LanceMcCarthy/CommonHelpers/main/src/CommonHelpers.Tests/ExtensionsTests/HttpClientExtensionsTests.cs";
             var reporter = new Progress<DownloadProgressArgs>();
 
             // Act
             reporter.ProgressChanged += (s, e) =>
             {
-                progress = e.PercentComplete;
+                progress = (int)e.PercentComplete;
             };
 
+            var client = new HttpClient();
             var result = await client.DownloadStringWithProgressAsync(url, reporter, cts.Token);
 
+            Trace.WriteLine($"Progress After Completion: {progress}");
+
             // Assert
-            Assert.IsTrue(progress > 0, "DownloadStringWithProgressAndCancellation - progress was not incremented.");
+            Assert.IsTrue(progress == 100, "DownloadStringWithProgressAndCancellation - progress did not reach 100% completion.");
             Assert.IsFalse(cts.Token.IsCancellationRequested, "Cancellation was incorrectly requested.");
             Assert.IsFalse(string.IsNullOrEmpty(result), "String result was null.");
+
+            client.Dispose();
         }
 
         [TestMethod]
@@ -63,23 +72,27 @@ namespace CommonHelpers.Tests.ExtensionsTests
         {
             // Arrange
             Stream result = null;
-            float progress = 0;
+            int progress = 0;
             const string url = "https://progressdevsupport.blob.core.windows.net/sampledocs/pdfviewer-overview.pdf";
             var reporter = new Progress<DownloadProgressArgs>();
 
             // Act
             reporter.ProgressChanged += (s, e) =>
             {
-                progress = e.PercentComplete;
+                progress = (int)e.PercentComplete;
             };
 
+            var client = new HttpClient();
             result = await client.DownloadStreamWithProgressAsync(url, reporter);
 
+            Trace.WriteLine($"Progress After Completion: {progress}");
+
             // Assert
-            Assert.IsTrue(progress > 0, "DownloadStreamWithProgress - progress was not incremented");
+            Assert.IsTrue(progress == 100, "DownloadStreamWithProgress - progress did not reach 100% completion");
             Assert.IsTrue(result != null, "Stream is null");
 
             await result.DisposeAsync();
+            client.Dispose();
         }
 
         [TestMethod]
@@ -88,24 +101,28 @@ namespace CommonHelpers.Tests.ExtensionsTests
             // Arrange
             Stream result = null;
             var cts = new CancellationTokenSource();
-            float progress = 0;
+            int progress = 0;
             const string url = "https://progressdevsupport.blob.core.windows.net/sampledocs/pdfviewer-overview.pdf";
             var reporter = new Progress<DownloadProgressArgs>();
 
             // Act
             reporter.ProgressChanged += (s, e) =>
             {
-                progress = e.PercentComplete;
+                progress = (int)e.PercentComplete;
             };
 
+            var client = new HttpClient();
             result = await client.DownloadStreamWithProgressAsync(url, reporter, cts.Token);
 
+            Trace.WriteLine($"Progress After Completion: {progress}");
+
             // Assert
-            Assert.IsTrue(progress > 0, "DownloadSteamWithProgressAndCancellation - progress was not incremented.");
+            Assert.IsTrue(progress == 100, "DownloadSteamWithProgressAndCancellation - progress did not reach 100% completiond.");
             Assert.IsFalse(cts.Token.IsCancellationRequested, "Cancellation was incorrectly requested.");
             Assert.IsTrue(result != null, "Stream is null");
 
             await result.DisposeAsync();
+            client.Dispose();
         }
     }
 }
