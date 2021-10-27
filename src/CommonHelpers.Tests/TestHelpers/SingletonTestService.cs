b7f90d6b2da974fde29977417e429cba4df7d332
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-10-26 19:07:11 -0600

    Add tests for Singleton

---
+++ src/CommonHelpers.Tests/TestHelpers/SingletonTestService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommonHelpers.Tests.TestHelpers
+{
+    public class SingletonTestService
+    {
+        public SingletonTestService()
+        {
+            InstanceId = DateTime.Now.Ticks;
+        }
+
+        public long InstanceId { get; set; }
+    }
+}
