a2dfcbab2925fdfc19ca88274848954667723e00
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-10-28 15:53:34 -0600

    Update HttpClientExtensionsTests.cs

--- src/CommonHelpers.Tests/ExtensionsTests/HttpClientExtensionsTests.cs
+++ src/CommonHelpers.Tests/ExtensionsTests/HttpClientExtensionsTests.cs
@@ -17,14 +17,9 @@ namespace CommonHelpers.Tests.ExtensionsTests
         {
             // Arrange
             const string url = "https://dvlup.blob.core.windows.net/general-app-files/StaticResources/LoremIpsum.txt";
-
             string stringResult = string.Empty;
-            Stream streamResult = null;
             int stringDownloadProgress = 0;
-            int streamDownloadProgress = 0;
-
             var stringDownloadReporter = new Progress<DownloadProgressArgs>();
-            var streamDownloadReporter = new Progress<DownloadProgressArgs>();
             using var client = new HttpClient();
 
             // Act
@@ -36,22 +31,10 @@ namespace CommonHelpers.Tests.ExtensionsTests
             stringResult = await client.DownloadStringWithProgressAsync(url, stringDownloadReporter);
 
             Trace.WriteLine($"String Progress After Completion: {stringDownloadProgress}");
-
-            streamDownloadReporter.ProgressChanged += (s, e) =>
-            {
-                streamDownloadProgress = Convert.ToInt32(e.PercentComplete);
-            };
-
-            streamResult = await client.DownloadStreamWithProgressAsync(url, streamDownloadReporter);
-
-            Trace.WriteLine($"Stream Progress After Completion: {streamDownloadProgress}");
-
+            
             // Assert
             Assert.IsTrue(stringDownloadProgress == 100, "Download String - progress did not reach 100% completion");
-            Assert.IsTrue(streamDownloadProgress == 100, "Download Stream - progress did not reach 100% completion");
-
             Assert.IsFalse(string.IsNullOrEmpty(stringResult), "String download was null or empty.");
-            Assert.IsTrue(streamResult.Length > 0, "Stream download does not contain any data.");
         }
     }
 }
