c46cf7527e6365dfda3a2658397c325f35fbdac0
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-03-17 16:33:23 -0400

    [maui] Experimental IncrementalLoadingCollection exploration

---
+++ src/CommonHelpers/Collections/IncrementalLoadingCollection.cs
@@ -0,0 +1,103 @@
+﻿using CommonHelpers.Collections.Interfaces;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommonHelpers.Collections
+{
+    public class IncrementalLoadingCollection<T> : ObservableCollection<T>, IIncrementalLoader
+    {
+        private readonly Func<CancellationToken, uint, Task<ObservableCollection<T>>> _func;
+        private uint _maxItems;
+        private bool _isInfinite;
+        private CancellationToken _token;
+
+        /// <summary>
+        /// Incremental scrolling list supported by UI controls that implement ISupportIncrementalLoading
+        /// </summary>
+        /// <param name="func"></param>
+        public IncrementalLoadingCollection(Func<CancellationToken, uint, Task<ObservableCollection<T>>> func)
+            : this(func, 0) { }
+
+        /// <summary>
+        /// Incremental scrolling list supported by UI controls that implement ISupportIncrementalLoading
+        /// </summary>
+        /// <param name="func">Task that retrieves the items</param>
+        /// <param name="maximumItems">Set to the maximum number of items to expect</param>
+        public IncrementalLoadingCollection(Func<CancellationToken, uint, Task<ObservableCollection<T>>> func, uint maximumItems)
+        {
+            _func = func;
+
+            if (maximumItems == 0) //Infinite
+            {
+                _isInfinite = true;
+            }
+            else
+            {
+                _maxItems = maximumItems;
+
+                _isInfinite = false;
+            }
+        }
+
+        public bool HasMoreItems
+        {
+            get
+            {
+                if (_token.IsCancellationRequested) 
+                    return false;
+
+                if (_isInfinite) 
+                    return true;
+
+                return this.Count < _maxItems;
+            }
+        }
+
+        public Task<uint> LoadMoreItemsAsync(uint count)
+        {
+            return Task.Run(() => this.InternalLoadMoreItemsAsync(_token, count), _token);
+        }
+
+        private async Task<uint> InternalLoadMoreItemsAsync(CancellationToken passedToken, uint count)
+        {
+            _token = passedToken;
+            var baseIndex = this.Count;
+            uint numberOfItemsToGet = 0;
+
+            if (!_isInfinite)
+            {
+                if (baseIndex + count < _maxItems)
+                {
+                    numberOfItemsToGet = count;
+                }
+                else
+                {
+                    numberOfItemsToGet = _maxItems - (uint) (baseIndex);
+                }
+            }
+            else
+            {
+                numberOfItemsToGet = count;
+            }
+
+            var tempList = await _func(passedToken, numberOfItemsToGet);
+
+            if (tempList.Count == 0) // stop incremental loading 
+            {
+                _maxItems = (uint)this.Count;
+                _isInfinite = false;
+            }
+            else
+            {
+                foreach (var item in tempList)
+                {
+                    this.Add(item);
+                }
+            }
+
+            return (uint)tempList.Count;
+        }
+    }
+}
---
+++ src/CommonHelpers/Collections/Interfaces/ISupportIncrementalLoading.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace CommonHelpers.Collections.Interfaces;
+
+public interface IIncrementalLoader
+{
+    /// <summary>
+    /// The task to be executed when loading more data
+    /// </summary>
+    /// <param name="count">The number of items to be fetched.</param>
+    /// <returns>The number of items that were successfully fetched.</returns>
+    Task<uint> LoadMoreItemsAsync(uint count);
+
+    /// <summary>
+    /// Gets a sentinel value that supports incremental loading implementations.
+    /// </summary>
+    /// <returns>**true** if additional unloaded items remain in the view; otherwise, **false**.</returns>
+    bool HasMoreItems { get; }
+}
