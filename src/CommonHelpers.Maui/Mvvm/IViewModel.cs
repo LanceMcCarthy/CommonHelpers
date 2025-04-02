39a985d3876ea85ec720de5a216bb2d8a39d93d1
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-04-02 11:57:19 -0400

    [maui] Add MVVM features

---
+++ src/CommonHelpers.Maui/Mvvm/IViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CommonHelpers.Maui.Mvvm;
+
+public interface IViewModel
+{
+    void OnAppearing();
+
+    void OnDisappearing();
+
+    void OnNavigatingFrom(NavigatingFromEventArgs args);
+
+    void OnNavigatedFrom(NavigatedFromEventArgs args);
+
+    void OnNavigatedTo(NavigatedToEventArgs args);
+
+    bool OnBackButtonRequested();
+}
---
+++ src/CommonHelpers.Maui/Mvvm/PageViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using CommonHelpers.Common;
+
+namespace CommonHelpers.Maui.Mvvm;
+
+public class PageViewModelBase : ViewModelBase, IViewModel
+{
+    public virtual void OnAppearing() {}
+
+    public virtual void OnDisappearing() {}
+
+    public virtual void OnNavigatingFrom(NavigatingFromEventArgs args){}
+
+    public virtual void OnNavigatedFrom(NavigatedFromEventArgs args){}
+
+    public virtual void OnNavigatedTo(NavigatedToEventArgs args){}
+
+    public virtual bool OnBackButtonRequested() => false;
+}
