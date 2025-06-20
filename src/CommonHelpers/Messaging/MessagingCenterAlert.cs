a5f83ca59d6fe2f73d112aa5425c5f55700a3f67
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-06-20 16:03:49 -0400

    Add new messaging and task completions helpers.

---
+++ src/CommonHelpers/Messaging/MessagingCenterAlert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommonHelpers.Messaging;
+
+public class MessagingCenterAlert : IMessagingCenterItem
+{
+    public string Title { get; set; } = "Title";
+
+    public string Message { get; set; } = "Message";
+
+    public string Cancel { get; set; } = "Cancel";
+        
+    public Action OnCompleted { get; set; }
+}
---
+++ src/CommonHelpers/Tasks/NotifyTaskCompletion.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace CommonHelpers.Tasks;
+
+public sealed class NotifyTaskCompletion<TResult> : INotifyPropertyChanged
+{
+    private readonly TResult _defaultResult;
+
+    public NotifyTaskCompletion(Task<TResult> task)
+    {
+        Task = task;
+        if (!task.IsCompleted)
+        {
+            var _ = WatchTaskAsync(task);
+        }
+    }
+
+    public NotifyTaskCompletion(Task<TResult> task, TResult defaultResult = default)
+    {
+        _defaultResult = defaultResult;
+        Task = task;
+
+        if (!task.IsCompleted)
+        {
+            var _ = WatchTaskAsync(task);
+        }
+    }
+
+    private async Task WatchTaskAsync(Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch
+        {
+        }
+
+        var propertyChanged = PropertyChanged;
+
+        if (propertyChanged == null)
+            return;
+
+        propertyChanged(this, new PropertyChangedEventArgs("Status"));
+        propertyChanged(this, new PropertyChangedEventArgs("IsCompleted"));
+        propertyChanged(this, new PropertyChangedEventArgs("IsNotCompleted"));
+
+        if (task.IsCanceled)
+        {
+            propertyChanged(this, new PropertyChangedEventArgs("IsCanceled"));
+        }
+        else if (task.IsFaulted)
+        {
+            propertyChanged(this, new PropertyChangedEventArgs("IsFaulted"));
+            propertyChanged(this, new PropertyChangedEventArgs("Exception"));
+            propertyChanged(this, new PropertyChangedEventArgs("InnerException"));
+            propertyChanged(this, new PropertyChangedEventArgs("ErrorMessage"));
+        }
+        else
+        {
+            propertyChanged(this, new PropertyChangedEventArgs("IsSuccessfullyCompleted"));
+            propertyChanged(this, new PropertyChangedEventArgs("Result"));
+        }
+    }
+    public Task<TResult> Task { get; private set; }
+
+    //public TResult Result => (Task.Status == TaskStatus.RanToCompletion) ? Task.Result : default(TResult);
+
+    public TResult Result => Task.Status == TaskStatus.RanToCompletion ? Task.Result : _defaultResult;
+
+    public TaskStatus Status => Task.Status;
+
+    public bool IsCompleted => Task.IsCompleted;
+
+    public bool IsNotCompleted => !Task.IsCompleted;
+
+    public bool IsSuccessfullyCompleted => Task.Status == TaskStatus.RanToCompletion;
+
+    public bool IsCanceled => Task.IsCanceled;
+
+    public bool IsFaulted => Task.IsFaulted;
+
+    public AggregateException Exception => Task.Exception;
+
+    public Exception InnerException => Exception?.InnerException;
+
+    public string ErrorMessage => InnerException?.Message;
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
